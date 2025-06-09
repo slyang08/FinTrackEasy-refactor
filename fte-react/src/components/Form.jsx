b94f973690ea0695cbce03e0380ca925627d5ab5
cleung66 <cleung66@myseneca.ca>
2025-06-09 00:45:45 -0400

    add shadcn form skeleton

---
+++ fte-react/src/components/Form.jsx
@@ -0,0 +1,317 @@
+"use client";
+// import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon, Check, ChevronsUpDown } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+
+const formSchema = z.object({
+    name_9540128754: z.string(),
+    txnDate: z.coerce.date(),
+    txnCategory: z.string(),
+    txnNote: z.string(),
+    txnAmount: z.string(),
+    txnRecurring: z.string().optional(),
+});
+
+export default function MyForm() {
+    const languages = [
+        { label: "English", value: "en" },
+        { label: "French", value: "fr" },
+        { label: "German", value: "de" },
+        { label: "Spanish", value: "es" },
+        { label: "Portuguese", value: "pt" },
+        { label: "Russian", value: "ru" },
+        { label: "Japanese", value: "ja" },
+        { label: "Korean", value: "ko" },
+        { label: "Chinese", value: "zh" },
+    ];
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            txnDate: new Date(),
+        },
+    });
+
+    const onSubmit = (values) => {
+        try {
+            console.log(values);
+            toast(
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                    <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+                </pre>
+            );
+        } catch (error) {
+            console.error("Form submission error", error);
+            toast.error("Failed to submit the form. Please try again.");
+        }
+    };
+
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8 max-w-3xl mx-auto py-10"
+            >
+                <div className="grid grid-cols-12 gap-4">
+                    <div className="col-span-6">
+                        <FormField
+                            control={form.control}
+                            name="name_9540128754"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            placeholder="e.g. shopping"
+                                            className="resize-none"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <div className="col-span-6">
+                        <FormField
+                            control={form.control}
+                            name="txnDate"
+                            render={({ field }) => (
+                                <FormItem className="flex flex-col">
+                                    <FormLabel>Date</FormLabel>
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <FormControl>
+                                                <Button
+                                                    variant="outline"
+                                                    className={cn(
+                                                        "w-[240px] pl-3 text-left font-normal",
+                                                        !field.value && "text-muted-foreground"
+                                                    )}
+                                                >
+                                                    {field.value
+                                                        ? format(field.value, "PPP")
+                                                        : "Pick a date"}
+                                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                </Button>
+                                            </FormControl>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0" align="start">
+                                            <Calendar
+                                                mode="single"
+                                                selected={field.value}
+                                                onSelect={field.onChange}
+                                                initialFocus
+                                            />
+                                        </PopoverContent>
+                                    </Popover>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                </div>
+
+                <FormField
+                    control={form.control}
+                    name="txnCategory"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                            <FormLabel>Category</FormLabel>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <FormControl>
+                                        <Button
+                                            variant="outline"
+                                            role="combobox"
+                                            className={cn(
+                                                "w-[200px] justify-between",
+                                                !field.value && "text-muted-foreground"
+                                            )}
+                                        >
+                                            {field.value
+                                                ? languages.find((l) => l.value === field.value)
+                                                      ?.label
+                                                : "Select language"}
+                                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                        </Button>
+                                    </FormControl>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-[200px] p-0">
+                                    <Command>
+                                        <CommandInput placeholder="Search language..." />
+                                        <CommandList>
+                                            <CommandEmpty>No language found.</CommandEmpty>
+                                            <CommandGroup>
+                                                {languages.map((language) => (
+                                                    <CommandItem
+                                                        value={language.label}
+                                                        key={language.value}
+                                                        onSelect={() => {
+                                                            form.setValue(
+                                                                "txnCategory",
+                                                                language.value
+                                                            );
+                                                        }}
+                                                    >
+                                                        <Check
+                                                            className={cn(
+                                                                "mr-2 h-4 w-4",
+                                                                language.value === field.value
+                                                                    ? "opacity-100"
+                                                                    : "opacity-0"
+                                                            )}
+                                                        />
+                                                        {language.label}
+                                                    </CommandItem>
+                                                ))}
+                                            </CommandGroup>
+                                        </CommandList>
+                                    </Command>
+                                </PopoverContent>
+                            </Popover>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="txnNote"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Note</FormLabel>
+                            <FormControl>
+                                <Textarea
+                                    placeholder="e.g. new sunglasses"
+                                    className="resize-none"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>30 character limit</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <div className="grid grid-cols-12 gap-4">
+                    <div className="col-span-6">
+                        <FormField
+                            control={form.control}
+                            name="txnAmount"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Amount</FormLabel>
+                                    <FormControl>
+                                        <Textarea className="resize-none" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+
+                    <div className="col-span-6">
+                        <FormField
+                            control={form.control}
+                            name="txnRecurring"
+                            render={({ field }) => (
+                                <FormItem className="flex flex-col">
+                                    <FormLabel>Recurrence (optional)</FormLabel>
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <FormControl>
+                                                <Button
+                                                    variant="outline"
+                                                    role="combobox"
+                                                    className={cn(
+                                                        "w-[200px] justify-between",
+                                                        !field.value && "text-muted-foreground"
+                                                    )}
+                                                >
+                                                    {field.value
+                                                        ? languages.find(
+                                                              (l) => l.value === field.value
+                                                          )?.label
+                                                        : "Select language"}
+                                                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                                </Button>
+                                            </FormControl>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-[200px] p-0">
+                                            <Command>
+                                                <CommandInput placeholder="Search language..." />
+                                                <CommandList>
+                                                    <CommandEmpty>No language found.</CommandEmpty>
+                                                    <CommandGroup>
+                                                        {languages.map((language) => (
+                                                            <CommandItem
+                                                                value={language.label}
+                                                                key={language.value}
+                                                                onSelect={() => {
+                                                                    form.setValue(
+                                                                        "txnRecurring",
+                                                                        language.value
+                                                                    );
+                                                                }}
+                                                            >
+                                                                <Check
+                                                                    className={cn(
+                                                                        "mr-2 h-4 w-4",
+                                                                        language.value ===
+                                                                            field.value
+                                                                            ? "opacity-100"
+                                                                            : "opacity-0"
+                                                                    )}
+                                                                />
+                                                                {language.label}
+                                                            </CommandItem>
+                                                        ))}
+                                                    </CommandGroup>
+                                                </CommandList>
+                                            </Command>
+                                        </PopoverContent>
+                                    </Popover>
+                                    <FormDescription>
+                                        This is the language that will be used in the dashboard.
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                </div>
+
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    );
+}
