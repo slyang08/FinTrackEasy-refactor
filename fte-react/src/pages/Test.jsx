75dd288845a13c0e4cfd8c59b871d9171f23b948
cleung66 <cleung66@myseneca.ca>
2025-06-15 19:07:14 -0400

    fix lint check

--- fte-react/src/pages/Test.jsx
+++ fte-react/src/pages/Test.jsx
@@ -1,7 +1,3 @@
-import React, { useState } from "react";
-
-import useFilteredIncomes from "@/hooks/useFilteredIncomes";
-
 export default function Test() {
     return <div className="p-4"></div>;
 }
