dabb8e263fed4f7c432ac70b45ff74a22f5df2b2
Tracy Tran <tran.tt.tracy@gmail.com>
2025-06-16 04:05:49 -0400

    updated background colour for contact page

--- fte-react/src/pages/ContactUs.jsx
+++ fte-react/src/pages/ContactUs.jsx
@@ -11,7 +11,6 @@ import { Textarea } from "@/components/ui/textarea";
 export default function ContactUs() {
     const [formData, setFormData] = useState({
         name: "",
-        phone: "",
         email: "",
         description: "",
     });
@@ -27,7 +26,6 @@ export default function ContactUs() {
     const handleClear = () => {
         setFormData({
             name: "",
-            phone: "",
             email: "",
             description: "",
         });
@@ -47,14 +45,14 @@ export default function ContactUs() {
     };
 
     return (
-        <div className="min-h-screen bg-white flex justify-center items-center px-4">
+        <div className="min-h-screen bg-[#F7F8F9] flex justify-center items-center px-4">
             <div className="max-w-5xl w-full bg-white rounded-xl shadow-xl overflow-hidden grid grid-cols-1 md:grid-cols-2">
                 {/* Left Side - Contact Info */}
                 <div className="bg-gray-100 pt-20 p-8">
                     <p className="text-medium font-semibold text-gray-800 mb-2">Connect</p>
                     <h2 className="text-5xl font-semibold mb-4">Get in Touch</h2>
                     <p className="text-gray-700 mb-6">
-                        We’re here to help you with your inquiries.
+                        We're here to help you with your inquiries.
                     </p>
                     <div className="space-y-4 text-base text-gray-800">
                         <p className="flex items-center gap-3">
@@ -66,7 +64,7 @@ export default function ContactUs() {
                         <p className="flex items-center gap-3">
                             <img src="/phone.svg" alt="Phone icon" width={22} height={22} />
                             <a href="tel:+15550000000" className="underline">
-                                +1 (555) 000–0000
+                                +1 (555) 000-0000
                             </a>
                         </p>
                         <p className="flex items-center gap-3">
