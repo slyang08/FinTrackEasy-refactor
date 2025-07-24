3238b25b55786e2e530ba9bebd41d38a7f744600
Yang <yang@YangdeMacBook-Pro-2.local>
2025-07-24 10:59:16 -0400

    feat(deploy): deploy frontend and backend to Render

--- fte-react/vite.config.js
+++ fte-react/vite.config.js
@@ -14,4 +14,13 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].[hash].js`,
+                chunkFileNames: `assets/[name].[hash].js`,
+                assetFileNames: `assets/[name].[hash].[ext]`,
+            },
+        },
+    },
 });
