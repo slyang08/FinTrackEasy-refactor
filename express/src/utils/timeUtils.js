32b04e771b7835559956a2e267ee5383c9d1015a
Yang <yang@mac.home>
2025-08-02 00:05:28 -0400

    hotfix(tz): hotfix time zone library donot suit in ES6

--- express/src/utils/timeUtils.js
+++ express/src/utils/timeUtils.js
@@ -1,9 +1,10 @@
 // express/src/utils/timeUtils.js
-import { format, utcToZonedTime } from "date-fns-tz";
+// import { format, utcToZonedTime } from "date-fns-tz";
+import { DateTime } from "luxon";
 
 const TIME_ZONE = "America/Toronto";
 
 export function formatToTorontoTime(dateISOString) {
-    const zonedDate = utcToZonedTime(dateISOString, TIME_ZONE);
-    return format(zonedDate, "yyyy-MM-dd HH:mm:ss", { timeZone: TIME_ZONE });
+    const zonedDate = DateTime.fromISO(dateISOString, { zone: TIME_ZONE });
+    return zonedDate.toFormat("yyyy-MM-dd HH:mm:ss");
 }
