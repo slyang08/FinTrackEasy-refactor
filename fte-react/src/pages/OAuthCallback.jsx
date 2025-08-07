3fb214c958b66d535b124ea4acc65dff89fc8961
Sheng-Lin Yang <141781844+slyang08@users.noreply.github.com>
2025-08-07 02:26:25 -0400

    fix(oauth): fix oauth success and jump to login (#94)
    Co-authored-by: Yang <yang@mac.home>

--- fte-react/src/pages/OAuthCallback.jsx
+++ fte-react/src/pages/OAuthCallback.jsx
@@ -1,10 +1,13 @@
 // src/pages/OAuthCallback.jsx
+import { useAtom } from "jotai";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import api from "@/api/axios";
+import { isAuthenticated } from "@/atoms/Atom";
 
 export default function OAuthCallback() {
+    const [, setIsAuth] = useAtom(isAuthenticated);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -21,19 +24,18 @@ export default function OAuthCallback() {
         api.post("/auth/set-cookie", { token })
             .then(() => {
                 // 3. Successfully set cookie, authenticate immediately (will bring up stateless cookie)
-                setTimeout(() => {
-                    return api.get("/auth/me");
-                }, 200);
+                return api.get("/auth/me");
             })
             .then(() => {
                 // 4. Get the user to store and navigate to the global state.
+                setIsAuth(true);
                 navigate("/overview");
             })
             .catch(() => {
                 // Failback login
                 navigate("/login");
             });
-    }, [navigate]);
+    }, [navigate, setIsAuth]);
 
     return <div>Logging you in...</div>;
 }
